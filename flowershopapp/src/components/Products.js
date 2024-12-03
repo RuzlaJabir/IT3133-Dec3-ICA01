8d09396d04aae8dd59a78921cd0c3384bc2dab67
RuzlaJabir <154322240+RuzlaJabir@users.noreply.github.com>
2024-12-03 18:13:58 +0000

    Added updateCart function to Products component

--- flowershopapp/src/components/Products.js
+++ flowershopapp/src/components/Products.js
@@ -9,22 +9,31 @@ export default function Products(){
    const [cart,setCart] = useState([]) //cart state
 
    //function to add item to cart
-   const addToCart = (flower) =>{
+   const addToCart = (product) =>{
    
     setCart((prevCart) => {
         //check if the item is already in the cart
-        const existingItem = prevCart.find((item) => item.id === flower.id);
+        const existingItem = prevCart.find((item) => item.id === product.id);
         if(existingItem){
             //Update the quantity
             return prevCart.map((item) =>
-                item.id === flower.id ? { ...item, qty: item.qty + 1} : item
+                item.id === product.id ? { ...item, qty: item.qty + 1} : item
             );
     }
     //Add new item to the cart
-    return [...prevCart, { ...flower, qty: 1}];
+    return [...prevCart, { ...product, qty: 1}];
 
    });
 };
+
+const updateCart = (id, newQty) => {
+    setCart((prevCart) =>
+        prevCart
+            .map((item) => (item.id === id ? { ...item, qty: newQty } : item))
+            .filter((item) => item.qty > 0) // Remove items with qty 0
+    );
+};
+
     return(
         <>
             <div className="item1">
@@ -51,7 +60,7 @@ export default function Products(){
             <div className="item3">
                 {
                 //cart
-                  <Cart cart={cart} /> //Pass cart as prop
+                  <Cart cart={cart} updateCart={updateCart}/> //Pass cart as prop
                 }
             </div>
         </>
